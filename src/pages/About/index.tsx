c2fcd6846f9e4658a8fee0a2fcf8770dda40d5ea
kennybc <54950614+kennybc@users.noreply.github.com>
2024-08-18 02:06:33 -0400

    populate pages and add basic heading/paragraph styles; todo: support deck transitions directly between pages

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -7,87 +7,70 @@ const About = () => {
   return (
     <Page>
       <Section>
-        <h2>Header</h2>
+        <h1>About & core principles.</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          finibus euismod ultricies. Phasellus quis pharetra nisl. Morbi a
-          faucibus ex, eget rhoncus nibh. Cras euismod vehicula vulputate. Donec
-          in augue lacinia, sollicitudin ipsum id, vestibulum nunc. Nam aliquet
-          augue nec tortor molestie, in ultrices felis tincidunt. Maecenas
-          molestie eros vel elit ornare imperdiet. Vestibulum efficitur, nisi in
-          sodales volutpat, est velit venenatis nulla, ac volutpat lorem justo
-          eget ex. Aliquam nec dui id lectus egestas tempor quis vitae est.
-          Mauris ex lorem, viverra eget malesuada ut, porttitor eu mi.
+          Arbor studio is an atmosphere design studio specializing in making
+          infrastructural art. At the core of our work is the desire to
+          collaborate on building imaginative & flexible infrastructures for
+          sustainable, relational worlds.
+        </p>
+        <h3>Core principles behind our design science:</h3>
+      </Section>
+      <Section>
+        <h2>Design is cosmotechnical and fractal.</h2>
+        <p>
+          Our approach considers design as cosmotechnical and fractal: The
+          smallest decision mirrors the total vision and each decision is
+          embedded within a larger cosmos of nested ecosystems, rippling through
+          the whole. Design must be aware of and responsible for many scales.
         </p>
       </Section>
       <Section>
-        <h2>Header</h2>
+        <h2>Ecological being requires technique and skillful design.</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          finibus euismod ultricies. Phasellus quis pharetra nisl. Morbi a
-          faucibus ex, eget rhoncus nibh. Cras euismod vehicula vulputate. Donec
-          in augue lacinia, sollicitudin ipsum id, vestibulum nunc. Nam aliquet
-          augue nec tortor molestie, in ultrices felis tincidunt. Maecenas
-          molestie eros vel elit ornare imperdiet. Vestibulum efficitur, nisi in
-          sodales volutpat, est velit venenatis nulla, ac volutpat lorem justo
-          eget ex. Aliquam nec dui id lectus egestas tempor quis vitae est.
-          Mauris ex lorem, viverra eget malesuada ut, porttitor eu mi.
+          We aim to become increasingly intentional about our designs and
+          decisions, knowing that each action or arrangement we make defines a
+          location and specific relation with everything around us—from the
+          smallest and most immediate atmosphere to the all-encompassing and
+          cosmic. Each design decision suggests and enacts a particular
+          politics, ethics, and philosophy of being in the world.
         </p>
       </Section>
       <Section>
-        <h2>Header</h2>
+        <h2>Precision in the way of poetry.</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          finibus euismod ultricies. Phasellus quis pharetra nisl. Morbi a
-          faucibus ex, eget rhoncus nibh. Cras euismod vehicula vulputate. Donec
-          in augue lacinia, sollicitudin ipsum id, vestibulum nunc. Nam aliquet
-          augue nec tortor molestie, in ultrices felis tincidunt. Maecenas
-          molestie eros vel elit ornare imperdiet. Vestibulum efficitur, nisi in
-          sodales volutpat, est velit venenatis nulla, ac volutpat lorem justo
-          eget ex. Aliquam nec dui id lectus egestas tempor quis vitae est.
-          Mauris ex lorem, viverra eget malesuada ut, porttitor eu mi.
+          Our approach to designs seeks precision in the way of poetry’s
+          precision: nondeterministic, emergent, and principled. We want to help
+          you choose precise tactics, in alignment with your principles and
+          ours, in a true collaboration.
         </p>
       </Section>
       <Section>
-        <h2>Header</h2>
+        <h2>Tactical engagement for intuitive, emergent work.</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          finibus euismod ultricies. Phasellus quis pharetra nisl. Morbi a
-          faucibus ex, eget rhoncus nibh. Cras euismod vehicula vulputate. Donec
-          in augue lacinia, sollicitudin ipsum id, vestibulum nunc. Nam aliquet
-          augue nec tortor molestie, in ultrices felis tincidunt. Maecenas
-          molestie eros vel elit ornare imperdiet. Vestibulum efficitur, nisi in
-          sodales volutpat, est velit venenatis nulla, ac volutpat lorem justo
-          eget ex. Aliquam nec dui id lectus egestas tempor quis vitae est.
-          Mauris ex lorem, viverra eget malesuada ut, porttitor eu mi.
+          We use precise and artistic tactics, manipulating specific variables,
+          using grounded variables as an anchor for a project vision that is
+          continuously emerging, to help you shape & co-create the atmosphere of
+          your work—whether that is physical or conceptual infrastructure,
+          though we find most projects to be a bit of both.
         </p>
       </Section>
       <Section>
-        <h2>Header</h2>
+        <h2>Art is infrastructure, infrastructure is art.</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          finibus euismod ultricies. Phasellus quis pharetra nisl. Morbi a
-          faucibus ex, eget rhoncus nibh. Cras euismod vehicula vulputate. Donec
-          in augue lacinia, sollicitudin ipsum id, vestibulum nunc. Nam aliquet
-          augue nec tortor molestie, in ultrices felis tincidunt. Maecenas
-          molestie eros vel elit ornare imperdiet. Vestibulum efficitur, nisi in
-          sodales volutpat, est velit venenatis nulla, ac volutpat lorem justo
-          eget ex. Aliquam nec dui id lectus egestas tempor quis vitae est.
-          Mauris ex lorem, viverra eget malesuada ut, porttitor eu mi.
+          We believe in the potential of art beyond the manner in which it is
+          being produced & consumed currently, captured by the neoliberal “art
+          market.” Art has been sequestered into a realm of the public in which
+          it can have no bearing on our built world—and, indeed, is understood
+          commonly as oppositional to the sciences, which are seen as
+          infrastructural and fundamental. We believe that art is, above all, a
+          skillful practice of catalytic rearrangement of existing elements to
+          produce the conditions for new & impossible beings, actions, and
+          systems to unfurl.
         </p>
-      </Section>
-      <Section>
-        <h2>Header</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-          finibus euismod ultricies. Phasellus quis pharetra nisl. Morbi a
-          faucibus ex, eget rhoncus nibh. Cras euismod vehicula vulputate. Donec
-          in augue lacinia, sollicitudin ipsum id, vestibulum nunc. Nam aliquet
-          augue nec tortor molestie, in ultrices felis tincidunt. Maecenas
-          molestie eros vel elit ornare imperdiet. Vestibulum efficitur, nisi in
-          sodales volutpat, est velit venenatis nulla, ac volutpat lorem justo
-          eget ex. Aliquam nec dui id lectus egestas tempor quis vitae est.
-          Mauris ex lorem, viverra eget malesuada ut, porttitor eu mi.
+          <i>The question of radical infrastructure</i> must necessarily deploy
+          art as a strategy to ask after new and unknown horizons.
         </p>
       </Section>
     </Page>
--- src/pages/Offerings/index.tsx
+++ src/pages/Offerings/index.tsx
@@ -1,7 +1,60 @@
+import Page from "@/components/Page";
+import Section from "@/components/Section";
+
 import "./index.css";
+import { Link } from "react-router-dom";
 
 const Offerings = () => {
-  return <>Offerings</>;
+  return (
+    <Page>
+      <Section>
+        <h1>Offerings.</h1>
+        <p>
+          The tactical forms that our design science take & past models of
+          collaboration:
+        </p>
+        <ol>
+          <li>
+            Atmosphere design and production &#123;including
+            workspace/collaborative space/makerspace design, set design,
+            immersive art installation design, exhibition & gallery design—&
+            production on all such fronts&#125;
+          </li>
+          <li>
+            Strategy consulting &#123;bringing our{" "}
+            <Link to="/about">philosophy</Link> and{" "}
+            <Link to="/about">design principles</Link> as an animating approach
+            to scaffolding new pathways for you & your organization through
+            strategic forms of thinking, decision making, and action&#125;
+          </li>
+          <li>
+            Sigil-making &#123;original symbolic forms/matter for many uses:
+            ritual, personal, company. Ex.&#x29; custom typesets, logos, tattoo
+            designs, protective talismans, mural painting, commissioned
+            art&#125;
+          </li>
+          <li>
+            Event production &#123;helping bring to life your existing vision as
+            a fully manifested happening—both short-term and long-term&#125;
+          </li>
+          <li>
+            Workshops &#123;bringing our philosophy to your area of interest
+            through hands-on and time-limited spaces of engagement,
+            collaboration, and co-learning&#125;
+          </li>
+        </ol>
+        <h3>Some possibilities.</h3>
+        <p>
+          To give you a sense of what work is possible together: designing and
+          building an immersive installation for an art exhibition, producing a
+          symposium, designing an office-space, designing and installing an
+          exhibition/gallery space, designing organizational structures,
+          developing conceptual frameworks for personal thinking,
+          imagining/directing the development of a company.
+        </p>
+      </Section>
+    </Page>
+  );
 };
 
 export default Offerings;
