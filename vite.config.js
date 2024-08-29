2a4503b979c153efefb1f07be3f69a7f67068ad4
kennybc <54950614+kennybc@users.noreply.github.com>
2024-08-29 11:51:40 -0400

    update vorlon; spa support; custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -10,5 +11,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "/arbor-studios/",
+  base: "/",
 });
